a2335f2201f27a7cc2f8bae27a6c6eb0a8e2fba2
Aditya Malviya <139367264+adityamalviya007@users.noreply.github.com>
2026-08-24 21:54:50 +0530

    Time: 1 ms (99.96%), Space: 94.4 MB (51.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -11,7 +11,7 @@ class Solution {
             if (price - minPrice > maxProfit){
                 maxProfit = price -minPrice;
             }
-          
+           
         }
 
         return maxProfit;
